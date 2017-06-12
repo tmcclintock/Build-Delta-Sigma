a91a5317f267a8ca57832a6ebda87df800ea6a8e
Tom McClintock <tmcclintock89@gmail.com>
2017-06-12 15:03:56 -0500

    Got rid of the old test data in favor of R and xi_hm from the Delta_Sigma code. Changed pymain to example since it makes more sense.

--- example.py
+++ example.py
@@ -24,11 +24,9 @@ import py_Build_Delta_Sigma
 import matplotlib.pyplot as plt
 import numpy as np
 
-test_path = "test_data/"
-
 #First load in the halo-matter CF from somewhere
-R = np.genfromtxt(test_path+"R.txt")
-xi_hm = np.genfromtxt(test_path+"xi_hm_z0.0_13.0_13.1.txt")
+R = np.genfromtxt("test_data/R.txt")
+xi_hm = np.genfromtxt("test_data/xi_hm.txt")
 
 #Create a dictionary with the cosmology
 cosmo = {"h":0.7,"om":0.3,"ok":0.0,"ode":0.7}
@@ -45,11 +43,9 @@ fmis - fracton of miscentered halos
 timing - 1 if you want to print timing information, 0 if not
 miscentering - 1 if you want to calculate miscentered information, 0 if not
 """
-input_params = {"Mass": 10**13.,"delta":200,"timing":1,\
-                    "Rmis":0.2,"fmis":0.3,"miscentering":1}
-#input_params["concentration"] = 5.0 #For eduardo's stuff
-input_params["concentration"] = 4.0*(input_params["Mass"]/5.e14)**-0.1
-#Above is an example M-c relation. This particular one is complete garbage.
+input_params = {"Mass": 3*10**14.,"delta":200,"timing":1,
+                "Rmis":0.25,"fmis":0.25,"miscentering":1}
+input_params["concentration"] = 5.0
 
 #Results come out in a dictionary
 return_dict = py_Build_Delta_Sigma.build_Delta_Sigma(R,xi_hm,cosmo,input_params)
@@ -62,6 +58,8 @@ delta_sigma = return_dict['delta_sigma']
 mis_sigma_r = return_dict['miscentered_sigma_r']
 mis_delta_sigma = return_dict['miscentered_delta_sigma']
 full_delta_sigma = return_dict['full_delta_sigma']
+
+np.savetxt("test_data/delta_sigma_BDS.txt", delta_sigma)
 
 #plt.loglog(R,xi_hm,label=r"$\xi_{hm}$")
 #plt.loglog(R,sigma_r,label=r"$\Sigma$")
